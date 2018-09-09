03f41b719b0c1be1f2ac71f6e0a38cd923d2f60e
Vincent <electricplasma411@gmail.com>
2018-09-09 18:20:13 -0400

    uploading file

---
+++ src/main.java
@@ -0,0 +1,10 @@
+class main {
+    public static void main(String[] args) {
+        name("Sam");
+        name("Timmy");
+    }
+
+    public static void name(String name) {
+        System.out.println("thank you " + name);
+    }
+}
