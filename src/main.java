2f49ea5b5f832e3dbd227c210297a4f90ef4f1f6
Vincent <electricplasma411@gmail.com>
2018-09-09 19:28:48 -0400

    method to introduce yourself

--- src/main.java
+++ src/main.java
@@ -2,10 +2,21 @@ import java.util.Scanner;
 
 class main {
     public static void main(String[] args) {
-       Scanner tech = new Scanner(System.in);
+        Scanner tech = new Scanner(System.in);
         System.out.println("tell me your name");
         String name = tech.nextLine();
         System.out.println("hello " + name);
+        System.out.println("How old are you?");
+        int age = tech.nextInt();
+        if (age <= 17){
+            System.out.println(age + " years old is not old");
+        } else {
+            System.out.println(age + " years old is decent");
+
+
+        }
+
+
     }
 
 }
